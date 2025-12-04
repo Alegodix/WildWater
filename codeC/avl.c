07fbc34bf37049a299cbae48dbfda72f153655d8
A.G. <alexandre.godineau@etu.cyu.fr>
2025-12-04 09:50:24 +0100

    Refactor AVL tree functions to use string ID

--- codeC/avl.c
+++ codeC/avl.c
@@ -1,26 +1,26 @@
 #include "avl.h"
 
-pArbre creerArbre() {
-	pAVL nv=malloc(sizeof(AVL));
-  	nv->u->ID=NULL;
-  	nv->u->capte=nv->u->conso=nv->u->capacitemax=0;
-	nv->fg=nv->fd=NULL;
-	nv->eq=0;
+pArbre creerArbre(char* ID) { //créée un arbre et lui assigne l'ID en paramètre + valeurs NULL autrement; 
+	pAVL nouveau = malloc(sizeof(AVL));
+  	nouveau->u->ID = ID;
+  	nouveau->u->capte = nouveau->u->conso = nv->u->capacitemax = 0;
+	nv->fg = nv->fd = NULL;
+	nv->eq = 0;
 	return nv;
 }
 
-int max(int a, int b) { //retourne le max entre a et b deux entiers
-	if(a > b) return a;
+int max(int a, int b) { //retourne le max entre a et b deux entiers; OK
+	if (a > b) return a;
 	else return b;
 }
 
-int min(int a, int b) { //même chose pour le minimum entre deux entiers
-	if(a =< b) return a;
+int min(int a, int b) { //même chose pour le minimum entre deux entiers; OK
+	if (a =< b) return a;
 	else return b;
 }
 
 
-pAVL rotationgauche(pAVL a) { //fais une rotation gauche de l'AVL entré en paramètre
+pAVL rotationgauche(pAVL a) { //fais une rotation gauche de l'AVL entré en paramètre; OK
     int eq_a = 0;
     int eq_p = 0;
     pAVL pivot = a->fd;
@@ -34,7 +34,7 @@ pAVL rotationgauche(pAVL a) { //fais une rotation gauche de l'AVL entré en para
     return a;
 }
 
-pAVL rotationdroite(pAVL a) {
+pAVL rotationdroite(pAVL a) { //fait une rotation droite de l'AVL entré en paramètre; OK
     int eq_a = 0;
     int eq_p = 0;
     pAVL pivot = a->fg;
@@ -48,16 +48,16 @@ pAVL rotationdroite(pAVL a) {
     return a;
 }
 
-pAVL doublerotationgauche(pAVL a) { // fait une double rotation gauche de l'AVL entré en paramètre
+pAVL doublerotationgauche(pAVL a) { // fait une double rotation gauche de l'AVL entré en paramètre; OK 
     a->fd = rotationdroite(a->fd);
     return rotationgauche(a);
 }
 
-pAVL doublerotationdroite(pAVL a){ // fait une double rotation gauche de l'AVL entré en paramètre
+pAVL doublerotationdroite(pAVL a){ // fait une double rotation gauche de l'AVL entré en paramètre; OK
     a->fg = rotationgauche(a->fg);
     return rotationdroite(a);
 }
-pAVL equilibrerAVL(pAVL a) { //équilibre l'AVL à l'aide des fctions de rotation si déséquilibre (vérifie si il y a déséquilibre aussi)
+pAVL equilibrerAVL(pAVL a) { //équilibre l'AVL à l'aide des fctions de rotation si déséquilibre (vérifie si il y a déséquilibre aussi); OK normalement
 	if (a != NULL) {
 		if (a->equilibre <= -2) {
 			if (a->fg->equilibre <= 0) {
@@ -76,16 +76,16 @@ pAVL equilibrerAVL(pAVL a) { //équilibre l'AVL à l'aide des fctions de rotatio
 	return a;
 }
 
-pAVL insertionAVL(pAVL a, int v, int* h) {
+pAVL insertionAVL(pAVL a, char* ID, int* h) { //insère un élément dans l'AVL INCOMPLET A MODIFIER 
 	if (a == NULL) {
 		*h = 1;
-		return creerArbre(v);
+		return creerArbre(ID);
 	}
-	if (v < a->value) {
-		a->fg = insertionAVL(a->fg, v, h);
+	if (ID < a->value) {
+		a->fg = insertionAVL(a->fg, ID, h);
 		*h = -*h;
 	} else if (v > a->value) {
-		a->fd = insertionAVL(a->fd, v, h);
+		a->fd = insertionAVL(a->fd, ID, h);
 	} else {
 		*h = 0;
 		return a;
