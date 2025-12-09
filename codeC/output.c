35609f756e60926a2491074a195f86637fe7537a
simedor <108340948+simedor@users.noreply.github.com>
2025-12-09 11:47:09 +0100

    Update output.c
    Mise à jour pour prendre en compte le mode (src, real, max), modifs dans afficherNoeud et dans les paramètres des fonctions

--- codeC/output.c
+++ codeC/output.c
@@ -3,19 +3,26 @@
 /*
 Ecrire une ligne de données pour une usine et gère la conversion d'unités
 */
-void afficherNoeud(Usine* u, FILE* fichier) {
-    double volume = u->volumeSource / 1000.0;
-    fprintf(fichier, "%s;%.3f\n", u->ID, volume);
+void afficherNoeud(Usine* u, FILE* fichier, char* mode) {
+    double valeur = 0;
+    if (strcmp(mode, "src") == 0) {
+        valeur = u->volumeSource;
+    } else if (strcmp(mode, "max") == 0) {
+        valeur = u->capacite;
+    } else if (strcmp(mode, "real") == 0) {
+        valeur = u->volumeTraite;
+    }
+    fprintf(fichier, "%s;%.3f\n", u->ID, volume / 1000.0);
 }
 
 /*
 Ecrire les identifiants dans l'ordre alphabétique inverse
 */
-void parcoursInfixeInverse(pAVL a, FILE* fichier) {
+void parcoursInfixeInverse(pAVL a, FILE* fichier, char* mode) {
   if (a != NULL) {
-    parcoursInfixeInverse(a->fd, fichier);
-    afficherNoeud(a->u, fichier);
-    parcoursInfixeInverse(a->fg, fichier);
+    parcoursInfixeInverse(a->fd, fichier, mode);
+    afficherNoeud(a->u, fichier, mode);
+    parcoursInfixeInverse(a->fg, fichier, mode);
   }
 }
 
@@ -29,7 +36,7 @@ void outputHistoSource(char* nomFichier, pAVL a) {
         exit(1);
     }
     fprintf(fichier, "identifier;source volume (M.m3.year-1)\n");
-    parcoursInfixeInverse(a, fichier);
+    parcoursInfixeInverse(a, fichier, "src");
     fclose(fichier);
 }
 
